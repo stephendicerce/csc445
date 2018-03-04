3f57670dec4f4749d50b5e027e0f3591a90966d9
Stephen DiCerce <sdicerce@oswego.edu>
2018-03-04 00:44:32 -0500

    updated UDPServer

--- homework1/src/UDPServer.java
+++ homework1/src/UDPServer.java
@@ -62,6 +62,7 @@ public class UDPServer {
                     System.out.println("IO Exception has occurred.");
                 }
             }
+            System.out.println(i);
         }
         socket.close();
     }
