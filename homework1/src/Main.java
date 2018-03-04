c7237eaee964cdd3eca3f739a09a30e489e1b0c6
Stephen DiCerce <sdicerce@oswego.edu>
2018-03-04 15:47:12 -0500

    updated main for throughput support

--- homework1/src/Main.java
+++ homework1/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         String type, hostname, communicationType;
         int port;
         boolean systemSet = false;
+        boolean tcpTypeBoolean = false;
         TCPClient client;
         TCPServer server;
         UDPClient udpClient;
@@ -17,36 +18,76 @@ public class Main {
         communicationType = kb.nextLine();
 
         if(communicationType.equalsIgnoreCase("tcp")) {
+            String tcpType;
             do {
-                System.out.println("Enter whether this computer is a server or a client.(Start the server first)");
-                type = kb.nextLine();
+                System.out.println("Do you want to measure throughput or simple echo?");
+                tcpType = kb.nextLine();
+                if (tcpType.equalsIgnoreCase("simple echo")) {
+                    tcpTypeBoolean = true;
+                    do {
+                        System.out.println("Enter whether this computer is a server or a client.(Start the server first)");
+                        type = kb.nextLine();
 
-                if (type.equalsIgnoreCase("client")) {
-                    systemSet = true;
-                    System.out.println("Please enter the hostname of the server you wish to connect to.");
-                    hostname = kb.nextLine();
-                    System.out.println("Enter the port number to operate on.");
-                    port = kb.nextInt();
-                    kb.nextLine();
-                    client = new TCPClient(hostname, port);
-                    if (client.openSocket())
-                        client.echo();
-                    else
-                        System.out.println("Since the socket couldn't be opened, the program will now exit.");
-                } else if (type.equalsIgnoreCase("server")) {
-                    systemSet = true;
-                    System.out.println("Please enter which port number the server should operate on.");
-                    port = kb.nextInt();
-                    kb.nextLine();
-                    server = new TCPServer(port);
-                    if (server.openSockets())
-                        server.listening(64, 1024);
-                    else
-                        System.out.println("Since the server couldn't be started, the program will now exit.");
-                } else {
-                    System.out.println("You didn't enter client or server. Please try again.");
-                }
-            } while (!systemSet);
+                        if (type.equalsIgnoreCase("client")) {
+                            systemSet = true;
+                            System.out.println("Please enter the hostname of the server you wish to connect to.");
+                            hostname = kb.nextLine();
+                            System.out.println("Enter the port number to operate on.");
+                            port = kb.nextInt();
+                            kb.nextLine();
+                            client = new TCPClient(hostname, port);
+                            if (client.openSocket())
+                                client.echo();
+                            else
+                                System.out.println("Since the socket couldn't be opened, the program will now exit.");
+                        } else if (type.equalsIgnoreCase("server")) {
+                            systemSet = true;
+                            System.out.println("Please enter which port number the server should operate on.");
+                            port = kb.nextInt();
+                            kb.nextLine();
+                            server = new TCPServer(port);
+                            if (server.openSockets())
+                                server.listening(64, 1024);
+                            else
+                                System.out.println("Since the server couldn't be started, the program will now exit.");
+                        } else {
+                            System.out.println("You didn't enter client or server. Please try again.");
+                        }
+                    } while (!systemSet);
+                    
+                } else if (tcpType.equalsIgnoreCase("measure throughput")) {
+                    tcpTypeBoolean = true;
+                    System.out.println("Enter whether this computer is a server or a client.(Start the server first)");
+                    type = kb.nextLine();
+
+                    if (type.equalsIgnoreCase("client")) {
+                        systemSet = true;
+                        System.out.println("Please enter the hostname of the server you wish to connect to.");
+                        hostname = kb.nextLine();
+                        System.out.println("Enter the port number to operate on.");
+                        port = kb.nextInt();
+                        kb.nextLine();
+                        client = new TCPClient(hostname, port);
+                        if (client.openSocket())
+                            client.measureTransferRates(1000, 16000, 64000, 256000, 1000000);
+                        else
+                            System.out.println("Since the socket couldn't be opened, the program will now exit.");
+                    } else if (type.equalsIgnoreCase("server")) {
+                        systemSet = true;
+                        System.out.println("Please enter which port number the server should operate on.");
+                        port = kb.nextInt();
+                        kb.nextLine();
+                        server = new TCPServer(port);
+                        if (server.openSockets())
+                            server.measureTransferRates(1000, 16000, 64000, 256000, 1000000);
+                        else
+                            System.out.println("Since the server couldn't be started, the program will now exit.");
+                    } else {
+                        System.out.println("You didn't enter client or server. Please try again.");
+                    }
+                } while (!systemSet);
+
+            } while(!tcpTypeBoolean);
 
         } else if(communicationType.equalsIgnoreCase("udp")) {
             do {
